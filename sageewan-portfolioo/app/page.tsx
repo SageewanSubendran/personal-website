179b065fa691145c116b88d9088c40ad148ee632
Sageewan Subendran <ssagee1@hotmail.com>
2026-03-10 22:42:34 -0400

    added profile pic

--- sageewan-portfolioo/app/page.tsx
+++ sageewan-portfolioo/app/page.tsx
@@ -25,7 +25,7 @@ export default function FounderPortfolio() {
       <section className="pt-40 pb-32 px-6 bg-white">
         <div className="max-w-5xl mx-auto text-center">
           <motion.img
-            src="/profile.jpg" // Replace with your image path
+            src="/profilePic.jpg" // Replace with your image path
             alt="Sageewan Subendran"
             initial={{ opacity: 0, scale: 0.9 }}
             animate={{ opacity: 1, scale: 1 }}
